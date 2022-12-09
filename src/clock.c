92afbcb9bd6abc409df2d30e43a4feb4e37d6b91
Gonzalo Araoz <gonzaloaraoz73@gmail.com>
2022-12-09 11:56:52 -0300

    TP5 Minutos decimales

--- src/clock.c
+++ src/clock.c
@@ -2,9 +2,17 @@
 #include <string.h>
 #define TIME_SIZE 6
 #define START_VALUE 0
+#define SECONDS_UNITS 5
+
+#define SECONDS_TENS 4
+#define MINUTE_UNITS 3
+#define MINUTE_TENS 2
+
 
 struct clock_s{
     bool valid;
+    uint16_t ticks_per_second;
+    uint16_t ticks_count;
     uint8_t time[TIME_SIZE];
 };
 
@@ -13,6 +21,8 @@ static struct clock_s instances;
 
 clock_t ClockCreate(uint16_t ticks_pers_second){
     instances.valid = false;
+    instances.ticks_count = START_VALUE;
+    instances.ticks_per_second = ticks_pers_second;
     memset(instances.time, START_VALUE, TIME_SIZE);
     return &instances;
 }
@@ -26,4 +36,25 @@ void ClockSetupTime(clock_t clock, uint8_t const * const time, uint8_t size) {
     memcpy(clock->time, time, size);
     clock->valid = true;
     
+}
+
+void ClockNewTicket(clock_t clock){
+    clock->ticks_count++;
+    if (clock->ticks_count == clock->ticks_per_second) {
+        clock->ticks_count=START_VALUE;
+        clock->time[SECONDS_UNITS]++;
+        if(clock->time[SECONDS_UNITS] == 10){
+            clock->time[SECONDS_UNITS] = 0;
+            clock->time[SECONDS_TENS]++;
+        }
+        if(clock->time[SECONDS_TENS] == 6){
+            clock->time[SECONDS_TENS] = 0;
+            clock->time[MINUTE_UNITS]++;
+            if(clock->time[MINUTE_UNITS] == 10){
+              clock->time[MINUTE_UNITS] = 0;
+              clock->time[MINUTE_TENS]++;
+            }
+        }
+        
+    }
 }
--- test/test_reloj.c
+++ test/test_reloj.c
@@ -20,12 +20,79 @@
 */
 #define TICKS_PER_SECOND 5
 
-void test_hora_inicial_invalida(void) {
+clock_t reloj;
+
+void SimulateSeconds(int seconds) {
+    for(int index = 0; index < seconds * TICKS_PER_SECOND; index ++) {
+        ClockNewTicket(reloj);
+    }
+
+}
+
+void setUp(void){
     static const uint8_t INICIAL[] = {1,2,3,4};
+    reloj = ClockCreate(TICKS_PER_SECOND);
+    ClockSetupTime(reloj,INICIAL, sizeof(INICIAL));
+}
+
+void test_sart_up(void) {
+    //static const uint8_t INICIAL[] = {1,2,3,4};
     static const uint8_t ESPERADO[] = {0,0,0,0,0,0};
     uint8_t hora[6];
     clock_t reloj = ClockCreate(TICKS_PER_SECOND);
-    ClockSetupTime(reloj,INICIAL, sizeof(INICIAL));
+    //ClockSetupTime(reloj,INICIAL, sizeof(INICIAL));
     TEST_ASSERT_FALSE(ClockGetTime(reloj, hora, sizeof(hora)));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora,sizeof(ESPERADO));
+}
+
+void test_set_up_curret_time(void){
+    static const uint8_t INICIAL[] = {1,2,3,4};
+    static const uint8_t ESPERADO[] = {1,2,3,4,0,0};
+    uint8_t hora[6];
+    clock_t reloj = ClockCreate(TICKS_PER_SECOND);
+    ClockSetupTime(reloj, INICIAL, sizeof(INICIAL));
+    TEST_ASSERT_TRUE(ClockGetTime(reloj, hora, sizeof(hora)));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora,sizeof(ESPERADO));
+
+}
+
+void test_one_second_elapsed(void){
+    //static const uint8_t INICIAL[] = {1,2,3,4};
+    static const uint8_t ESPERADO[] = {1,2,3,4,0,1};
+    uint8_t hora[6];
+
+    //clock_t reloj = ClockCreate(TICKS_PER_SECOND);
+    //ClockSetupTime(reloj,INICIAL, sizeof(INICIAL));
+    //for(int index = 0; index < TICKS_PER_SECOND; index ++) {
+    //    ClockNewTicket(reloj);
+    //}
+    SimulateSeconds(1);
+    ClockGetTime(reloj, hora, sizeof(hora));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora,sizeof(ESPERADO));
+}
+
+void test_ten_second_elapsed(void){
+    
+    static const uint8_t ESPERADO[] = {1,2,3,4,1,0};
+    uint8_t hora[6];
+    SimulateSeconds(10);
+    ClockGetTime(reloj, hora, sizeof(hora));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora,sizeof(ESPERADO));
+}
+
+void test_one_minute_elapsed(void){
+    
+    static const uint8_t ESPERADO[] = {1,2,3,5,0,0};
+    uint8_t hora[6];
+    SimulateSeconds(60);
+    ClockGetTime(reloj, hora, sizeof(hora));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora,sizeof(ESPERADO));
+}
+void test_ten_minute_elapsed(void){
+    
+    static const uint8_t ESPERADO[] = {1,2,4,4,0,0};
+    uint8_t hora[6];
+    SimulateSeconds(10 * 60);
+    ClockGetTime(reloj, hora, sizeof(hora));
     TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora,sizeof(ESPERADO));
 }
